6feeb5b5a3de73d89ff132466f6d845f18ffe38b
ErikPalvolgyi <101992471+ErikPalvolgyi@users.noreply.github.com>
2024-09-11 17:18:53 +0200

    Update UserInput.cs

--- UserInput.cs
+++ UserInput.cs
@@ -2,7 +2,6 @@ namespace TicTacToe
 {
     class UserInput
     {
-        public bool quit = false;
         public int rowFinal = 0;
         public int columnFinal = 0;
         public void TakeInput(string inputChar)
@@ -11,11 +10,6 @@ namespace TicTacToe
             string column = Console.ReadLine();
             Console.WriteLine($"Melyik sorba tegyem az {inputChar}-t (1-3)?");
             string row = Console.ReadLine();
-            if(column == ":q" || row == ":q")
-            {
-                quit = true;
-                return;
-            }
             rowFinal = int.Parse(row);
             columnFinal = int.Parse(column);
         }
